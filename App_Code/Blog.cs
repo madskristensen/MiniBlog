070fecce653a5fb2bfb488c90e0ddabc5e1e33d4
Mads Kristensen <post@madskristensen.net>
2013-05-19 16:31:01 -0700

    Fixed client-side perf issues

--- App_Code/Blog.cs
+++ App_Code/Blog.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
 using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
 
 public class Blog
 {
@@ -20,5 +24,26 @@ public class Blog
             return page;
 
         return 1;
+    }
+
+    public static string InsertLink(string rootRelativePath)
+    {
+        if (HttpRuntime.Cache[rootRelativePath] == null)
+        {
+            string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
+
+            if (!File.Exists(absolute))
+            {
+                throw new FileNotFoundException("File not found", absolute);
+            }
+
+            DateTime date = File.GetLastWriteTime(absolute);
+            int index = rootRelativePath.LastIndexOf('/');
+
+            string result = rootRelativePath.Insert(index, "/v-" + date.Ticks);
+            HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+        }
+
+        return HttpRuntime.Cache[rootRelativePath] as string;
     }
 }
