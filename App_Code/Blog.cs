d67db49828b8adf0a65540b12f6258ecdf0c8f03
Mads Kristensen <post@madskristensen.net>
2013-05-19 00:35:49 -0700

    Added microdata

--- App_Code/Blog.cs
+++ App_Code/Blog.cs
@@ -1,60 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Configuration;
 using System.Web;
-using System.Web.Hosting;
-using System.Xml.Linq;
 
 public class Blog
 {
-    public static List<Post> Posts = new List<Post>();
-
-    static Blog()
+    public static string Title
     {
-        foreach (string file in Directory.GetFiles(HostingEnvironment.MapPath("~/App_Data/"), "*.xml"))
-        {
-            XElement doc = XElement.Load(file);
-
-            Post post = new Post()
-            {
-                Title = doc.Element("title").Value,
-                Slug = doc.Element("slug").Value,
-                Content = doc.Element("content").Value,
-                PubDate = DateTime.Parse(doc.Element("pubDate").Value)
-            };
-
-            Posts.Add(post);
-        }
-
-        Posts = Posts.OrderByDescending(p => p.PubDate).ToList();
+        get { return ConfigurationManager.AppSettings.Get("blog:name"); }
     }
 
-    public static IEnumerable<Post> GetPosts(int postsPerPage)
+    public static string Theme
     {
-        HttpRequest request = HttpContext.Current.Request;
-        string slug = request.QueryString["slug"];
-
-        if (!string.IsNullOrEmpty(slug))
-        {
-            var post = Posts.FirstOrDefault(p => p.Slug.Equals(slug.Trim(), StringComparison.OrdinalIgnoreCase));
-
-            if (post == null)
-                throw new HttpException(404, "Blog post does not exist");
-
-            return new[] { post };
-        }
-
-        int page = 0;
-        int.TryParse(request.QueryString["page"], out page);
-
-        return Posts.Skip(postsPerPage * (page - 1)).Take(postsPerPage);
+        get { return ConfigurationManager.AppSettings.Get("blog:theme"); }
     }
-}
 
-public static class ExtensionMethods
-{
-    public static int CurrentPage(this HttpRequestBase request)
+    public static int CurrentPage(HttpRequestBase request)
     {
         int page = 0;
         if (int.TryParse(request.QueryString["page"], out page))
@@ -62,12 +21,4 @@ public static class ExtensionMethods
 
         return 1;
     }
-}
-
-public class Post
-{
-    public string Title { get; set; }
-    public string Slug { get; set; }
-    public string Content { get; set; }
-    public DateTime PubDate { get; set; }
 }
---
+++ App_Code/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Xml.Linq;
+
+public class Post
+{
+    public static List<Post> Posts = new List<Post>();
+
+    static Post()
+    {
+        foreach (string file in Directory.GetFiles(HostingEnvironment.MapPath("~/App_Data/"), "*.xml"))
+        {
+            XElement doc = XElement.Load(file);
+
+            Post post = new Post()
+            {
+                ID = Path.GetFileNameWithoutExtension(file),
+                Title = doc.Element("title").Value,
+                Slug = doc.Element("slug").Value.Replace(" ", "-").ToLowerInvariant(),
+                Content = doc.Element("content").Value,
+                PubDate = DateTime.Parse(doc.Element("pubDate").Value),
+                LastModified = DateTime.Parse(doc.Element("lastModified").Value),
+            };
+
+            Posts.Add(post);
+        }
+
+        Posts = Posts.OrderByDescending(p => p.PubDate).ToList();
+    }
+
+    public string ID { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string Content { get; set; }
+    public DateTime PubDate { get; set; }
+    public DateTime LastModified { get; set; }
+
+    public static IEnumerable<Post> GetPosts(int postsPerPage)
+    {
+        HttpRequest request = HttpContext.Current.Request;
+        string slug = request.QueryString["slug"];
+
+        if (!string.IsNullOrEmpty(slug))
+        {
+            var post = Posts.FirstOrDefault(p => p.Slug.Equals(slug.Trim(), StringComparison.OrdinalIgnoreCase));
+
+            if (post == null)
+                throw new HttpException(404, "Blog post does not exist");
+
+            return new[] { post };
+        }
+
+        int page = Blog.CurrentPage(new HttpRequestWrapper(request));
+
+        return Posts.Skip(postsPerPage * (page - 1)).Take(postsPerPage);
+    }
+}
