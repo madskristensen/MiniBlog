8fd849a85bd60302bde009cff8de57741625451e
Mads Kristensen <post@madskristensen.net>
2013-05-20 08:19:47 -0700

    Added beginning of editing

---
+++ Admin/edit.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" />
+/// <reference path="wysiwyg.js" />
+
+(function ($) {
+
+    var txtTitle, txtContent, btnSave, btnCancel;
+
+    $(function () {
+        
+        txtTitle = $("[itemprop~='name']");
+        txtContent = $("[itemprop~='articleBody']");        
+
+        txtTitle.attr('contentEditable', true);
+        txtContent.attr('contentEditable', true);
+
+        btnSave = $("#btnSave");
+        btnCancel = $("#btnCancel");
+
+        btnCancel.bind("click", function (e) {
+            location.href = location.href.replace("/edit/", "/").replace("/create/", "/");
+        });
+    });
+
+})(jQuery);
