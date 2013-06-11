5ac115ed846ea775e82a536feb49b6f1093be42f
Mads Kristensen <post@madskristensen.net>
2013-06-11 00:51:11 -0700

    Cleanup

--- app_code/handlers/MinifyHandler.cs
+++ app_code/handlers/MinifyHandler.cs
@@ -2,44 +2,59 @@
 using System;
 using System.IO;
 using System.Web;
+using System.Web.Caching;
 
 public class MinifyHandler : IHttpHandler
 {
     public void ProcessRequest(HttpContext context)
     {
         string file = context.Server.MapPath(context.Request.CurrentExecutionFilePath);
-        
-        Minify(context, file);
+        string extension = Path.GetExtension(file);
 
-        context.Response.Cache.SetLastModified(File.GetLastWriteTimeUtc(file));
-        context.Response.Cache.SetValidUntilExpires(true);
-        context.Response.Cache.SetExpires(DateTime.Now.AddYears(1));
-        context.Response.Cache.SetCacheability(HttpCacheability.Public);
+        if (context.IsDebuggingEnabled)
+        {
+            context.Response.TransmitFile(file);
+        }
+        else
+        {
+            Minify(context.Response, file, extension);
+        }
+
+        SetHeaders(context.Response, file, extension);
     }
 
-    private static void Minify(HttpContext context, string file)
+    private static void SetHeaders(HttpResponse response, string file, string extension)
     {
-        string extension = Path.GetExtension(file);
+        if (extension == ".css")
+        {
+            response.ContentType = "text/css";
+        }
+        else if (extension == ".js")
+        {
+            response.ContentType = "text/javascript";
+        }
+
+        response.Cache.SetLastModified(File.GetLastWriteTimeUtc(file));
+        response.Cache.SetValidUntilExpires(true);
+        response.Cache.SetExpires(DateTime.Now.AddYears(1));
+        response.Cache.SetCacheability(HttpCacheability.Public);
+        response.AddCacheDependency(new CacheDependency(file));
+    }
+
+    private static void Minify(HttpResponse response, string file, string extension)
+    {
         string content = File.ReadAllText(file);
         Minifier minifier = new Minifier();
 
         if (extension == ".css")
         {
-            CssSettings settings = new CssSettings();
-            settings.CommentMode = CssComment.None;
-
-            string result = minifier.MinifyStyleSheet(content, settings);
-            context.Response.Write(result);
-            context.Response.ContentType = "text/css";
+            CssSettings settings = new CssSettings() { CommentMode = CssComment.None };
+            response.Write(minifier.MinifyStyleSheet(content, settings));
         }
         else if (extension == ".js")
         {
-            CodeSettings settings = new CodeSettings();
-            settings.PreserveImportantComments = false;
-
-            string result = minifier.MinifyJavaScript(content, settings);
-            context.Response.Write(result);
-            context.Response.ContentType = "text/javascript";
+            CodeSettings settings = new CodeSettings() { PreserveImportantComments = false };
+            response.Write(minifier.MinifyJavaScript(content, settings));
         }
     }
 
