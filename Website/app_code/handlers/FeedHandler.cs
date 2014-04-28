d39df5751420ce1c1dcf0f1fae82bb208df265ca
Jan de Vries <jan@jan-v.nl>
2014-04-28 14:39:56 +0200

    Tapir unlimited rss

--- Website/app_code/handlers/FeedHandler.cs
+++ Website/app_code/handlers/FeedHandler.cs
@@ -6,54 +6,56 @@ using System.Xml;
 
 public class FeedHandler : IHttpHandler
 {
-    public void ProcessRequest(HttpContext context)
-    {
-        SyndicationFeed feed = new SyndicationFeed()
-        {
-            Title = new TextSyndicationContent(Blog.Title),
-            Description = new TextSyndicationContent("Latest blog posts"),
-            BaseUri = new Uri(context.Request.Url.Scheme + "://" + context.Request.Url.Authority),
-            Items = GetItems(),
-        };
+	public void ProcessRequest(HttpContext context)
+	{
+		bool getAllItems = context.Request.Url.PathAndQuery.Contains("tapir");
+		SyndicationFeed feed = new SyndicationFeed()
+		{
+			Title = new TextSyndicationContent(Blog.Title),
+			Description = new TextSyndicationContent("Latest blog posts"),
+			BaseUri = new Uri(context.Request.Url.Scheme + "://" + context.Request.Url.Authority),
+			Items = GetItems(getAllItems),
+		};
 
-        feed.Links.Add(new SyndicationLink(feed.BaseUri));
+		feed.Links.Add(new SyndicationLink(feed.BaseUri));
 
-        using (var writer = new XmlTextWriter(context.Response.Output))
-        {
-            var formatter = GetFormatter(context, feed);
-            formatter.WriteTo(writer);
-        }
+		using (var writer = new XmlTextWriter(context.Response.Output))
+		{
+			var formatter = GetFormatter(context, feed);
+			formatter.WriteTo(writer);
+		}
 
-        context.Response.ContentType = "text/xml";
-    }
+		context.Response.ContentType = "text/xml";
+	}
 
-    private IEnumerable<SyndicationItem> GetItems()
-    {
-        foreach (Post p in Blog.GetPosts())
-        {
-            var item = new SyndicationItem(p.Title, p.Content, p.AbsoluteUrl, p.AbsoluteUrl.ToString(), p.LastModified);
-            item.Authors.Add(new SyndicationPerson("", p.Author, ""));
-            yield return item;
-        }
-    }
+	private IEnumerable<SyndicationItem> GetItems(bool getAllItems)
+	{
+		int numberOfItems = getAllItems ? 0 : 20;
+		foreach (Post p in Blog.GetPosts(numberOfItems))
+		{
+			var item = new SyndicationItem(p.Title, p.Content, p.AbsoluteUrl, p.AbsoluteUrl.ToString(), p.LastModified);
+			item.Authors.Add(new SyndicationPerson("", p.Author, ""));
+			yield return item;
+		}
+	}
 
-    private SyndicationFeedFormatter GetFormatter(HttpContext context, SyndicationFeed feed)
-    {
-        string path = context.Request.Path.Trim('/');
-        int index = path.LastIndexOf('/');
+	private SyndicationFeedFormatter GetFormatter(HttpContext context, SyndicationFeed feed)
+	{
+		string path = context.Request.Path.Trim('/');
+		int index = path.LastIndexOf('/');
 
-        if (index > -1 && path.Substring(index + 1) == "atom")
-        {
-            context.Response.ContentType = "application/atom+xml";
-            return new Atom10FeedFormatter(feed);
-        }
+		if (index > -1 && path.Substring(index + 1) == "atom")
+		{
+			context.Response.ContentType = "application/atom+xml";
+			return new Atom10FeedFormatter(feed);
+		}
 
-        context.Response.ContentType = "application/rss+xml";
-        return new Rss20FeedFormatter(feed);
-    }
+		context.Response.ContentType = "application/rss+xml";
+		return new Rss20FeedFormatter(feed);
+	}
 
-    public bool IsReusable
-    {
-        get { return false; }
-    }
+	public bool IsReusable
+	{
+		get { return false; }
+	}
 }
