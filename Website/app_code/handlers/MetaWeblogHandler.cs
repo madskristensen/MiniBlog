e87db4ee9b6af9a801480994021541102526d3b2
Mads Kristensen <post@madskristensen.net>
2014-06-15 11:11:57 -0700

    Adding excerpt support to WLW post updates

--- Website/app_code/handlers/MetaWeblogHandler.cs
+++ Website/app_code/handlers/MetaWeblogHandler.cs
@@ -63,6 +63,7 @@ public class MetaWeblogHandler : XmlRpcService, IMetaWeblog
         if (match != null)
         {
             match.Title = post.Title;
+            match.Excerpt = post.Excerpt;
             match.Content = post.Content;
             match.Slug = post.Slug;
             match.Categories = post.Categories;
