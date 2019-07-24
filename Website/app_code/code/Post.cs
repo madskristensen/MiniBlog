2dd1091dde4e11b0c688e15e0143673460e47396
Jan de Vries <jan@jan-v.nl>
2019-07-24 11:21:10 +0200

    Changed RSS feed to default Only showing first paragraph in index

--- Website/app_code/code/Post.cs
+++ Website/app_code/code/Post.cs
@@ -81,33 +81,38 @@ public class Post
 
     public string GetHtmlContent()
     {
-        string result = Content;
-
-        // Youtube content embedded using this syntax: [youtube:xyzAbc123]
-        var video = "<div class=\"video\"><iframe src=\"//www.youtube.com/embed/{0}?modestbranding=1&amp;theme=light\" allowfullscreen></iframe></div>";
-        result = Regex.Replace(result, @"\[youtube:(.*?)\]", (Match m) => string.Format(video, m.Groups[1].Value));
-
-        // Images replaced by CDN paths if they are located in the /posts/ folder
-        var cdn = ConfigurationManager.AppSettings.Get("blog:cdnUrl");
-        var root = ConfigurationManager.AppSettings.Get("blog:path") + "/posts/";
-
-        if (!root.StartsWith("/"))
-            root = "/" + root;
-
-        result = Regex.Replace(result, "<img.*?src=\"([^\"]+)\"", (Match m) =>
-        {
-            string src = m.Groups[1].Value;
-            int index = src.IndexOf(root);
-
-            if (index > -1)
-            {
-                string clean = src.Substring(index);
-                return m.Value.Replace(src, cdn + clean);
-            }
-
-            return m.Value;
-        });
-
-        return result;
+		return GetHtmlContent(Content);
     }
+
+	public string GetHtmlContent(string content)
+	{
+		string result = content;
+
+		// Youtube content embedded using this syntax: [youtube:xyzAbc123]
+		var video = "<div class=\"video\"><iframe src=\"//www.youtube.com/embed/{0}?modestbranding=1&amp;theme=light\" allowfullscreen></iframe></div>";
+		result = Regex.Replace(result, @"\[youtube:(.*?)\]", (Match m) => string.Format(video, m.Groups[1].Value));
+
+		// Images replaced by CDN paths if they are located in the /posts/ folder
+		var cdn = ConfigurationManager.AppSettings.Get("blog:cdnUrl");
+		var root = ConfigurationManager.AppSettings.Get("blog:path") + "/posts/";
+
+		if (!root.StartsWith("/"))
+			root = "/" + root;
+
+		result = Regex.Replace(result, "<img.*?src=\"([^\"]+)\"", (Match m) =>
+		{
+			string src = m.Groups[1].Value;
+			int index = src.IndexOf(root);
+
+			if (index > -1)
+			{
+				string clean = src.Substring(index);
+				return m.Value.Replace(src, cdn + clean);
+			}
+
+			return m.Value;
+		});
+
+		return result;
+	}
 }
